6cf0bfdb5780c8ca5d9c843238be0364ddbaa6db
1a3g9f <129680604+1a3g9f@users.noreply.github.com>
2026-04-13 20:41:23 +0530

    Add files via upload

---
+++ personal_intro.py
@@ -0,0 +1,8 @@
+#user inputs
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+hobby = input("what is your hobby? ")
+#printing statements with f string
+print(f"WELCOME '{name}")
+print(f"You are {age} years old and I love doing {hobby}ad")
+
